01be82de2d71618ef09a269b32eca925650d7718
Chris Boot <chris@bootcom.co.uk>
2024-01-03 22:47:12 +0000

    #7 - Ensure that we can perform CRUD operations for future payments. Ensure that where future payments do not exist that we display appropriate content

---
+++ Mobile/Mobile/Common/Convertors/EditableFuturePaymentCanSaveConvertor.cs
@@ -0,0 +1,24 @@
+﻿using Models.Local;
+using System.Globalization;
+
+namespace Mobile.Common.Convertors
+{
+    public class EditableFuturePaymentCanSaveConvertor : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var dateTime = values[0] as DateTime?;
+            var detail = values[1] as string;
+            var amount = values[2] as double?;
+            var isProcessing = values[3] as bool?;
+
+            return dateTime > DateTime.Now && !string.IsNullOrWhiteSpace(detail) && (amount != null && amount > 0) && (isProcessing != null && isProcessing == false); 
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
