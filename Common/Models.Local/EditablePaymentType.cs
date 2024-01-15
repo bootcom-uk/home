0829f8c22a5955db5e14cffca611b3d33ca90fdd
Chris Boot <chris@bootcom.co.uk>
2024-01-15 22:17:46 +0000

    #10 - Offer the ability to modify payment type information

---
+++ Common/Models.Local/EditablePaymentType.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MongoDB.Bson;
+
+namespace Models.Local
+{
+    public partial class EditablePaymentType : ObservableObject
+    {
+
+        [ObservableProperty]
+        ObjectId id;
+
+        [ObservableProperty]
+        ObjectId? paymentCategory;
+
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        double paymentAmount;
+
+        [ObservableProperty]
+        int color;
+
+        [ObservableProperty]
+        bool havePaymentsEnded;
+
+        [ObservableProperty]
+        bool isResourceRequired;
+
+    }
+}
