837fb898cd4c46d0cd1c750cc87d050d78493e9f
Chris Boot <chris@bootcom.co.uk>
2024-01-16 20:31:35 +0000

    #10 - Add some tests

---
+++ Tests/HomeTests/SelectablePaymentTypeTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.Local;
+
+namespace HomeTests
+{
+    [TestFixture]
+    public class SelectablePaymentTypeTest
+    {
+
+        [Test()]
+        public void IsNameOk()
+        {
+            var selectablePayment = new SelectablePaymentType()
+            {
+                Name = "Chris"
+            };
+            Assert.IsFalse(string.IsNullOrWhiteSpace(selectablePayment.Name));
+        }
+
+    }
+}
